266a03916c07f4e880dbb11fc48a2ce6faa01563
Carlos Robert <crobertdmelo@gmail.com>
2023-05-25 21:38:38 -0300

    Adicionado - Configurações para uso de variáveis ambiente | Refatoração connect.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// Executando função de conexão ao MongoDB
-require('./db/connect');
-
 // Importando e executando o express
 const express = require('express');
 const app = express();
@@ -8,11 +5,28 @@ const app = express();
 // Importando arquivo de rotas
 const tasks = require('./routes/tasks');
 
+// Executando função de conexão ao MongoDB
+const connectDB = require('./db/connect');
+
+// Habilitando o uso das variáveis ambiente
+require('dotenv').config();
+
 // Executando os Middlewares
 app.use(express.json());
 app.use('/api/v1/tasks', tasks);
 
 
 // Rodando o servidor
-const port = 3000;
-app.listen(port, console.log(`- Server is listening on port ${port}`));
+const port = process.env.PORT;
+
+const start = async() => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log('- MongoDB connected');
+    app.listen(port, console.log(`- Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://roberttmello:%40Jovita14%40@taskmanager.3ymg8tt.mongodb.net/TASK-MANAGER-DB?retryWrites=true&w=majority';
+const connectDB = (url) => {
+	return mongoose.connect(url); 
+}
 
-mongoose.connect(connectionString)
-        .then(()=> console.log('- MongoDB connected'))
-        .catch((err)=> console.log(err));
+module.exports = connectDB;
+        
 
