c94af60a6e0b23e888dcee2ad46990f33eedd72b
Carlos Robert <crobertdmelo@gmail.com>
2023-05-31 12:24:14 -0300

    Adicionado - Validação básica para o modelo de Tasks

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean
+  name: {
+    type: String,
+    required: [true, 'Você precisa cadastrar o "nome"'],
+    trim: true,
+    maxlength: [100, 'O nome não pode ter mais que 100 caracteres']
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
